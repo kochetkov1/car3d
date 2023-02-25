f384c5e12f9693bfc866074b1614ccfccd17b3df
Dmitry Kochetkov <d.kochetkov1@yandex.ru>
2023-02-25 16:20:37 +0400

    first up

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/5JbEcQ9VRRYqZX6l/scene.splinecode');
